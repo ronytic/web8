32a9975921edb1d295115d4383c6064b6b8b4b6c
Ronald Nina <ronald.nina.layme@gmail.com>
2020-09-05 16:28:16 -0400

    Removiendo el index.php

--- application/views/saludar.php
+++ application/views/saludar.php
@@ -2,5 +2,8 @@
 
 
 
+<img src="<?php echo base_url(); ?>imagenes/manzana.jpg">
+
+
 <!-- http://localhost/web8/index.php/welcome/despedir  -->
 <a href="<?php echo base_url(); ?>welcome/despedir">IR A DESPEDIR</a>
